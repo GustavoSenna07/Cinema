302ec221492702e737cc121adee11f93f8d459b3
GustavoSenna07 <gustavo.psenna@gmail.com>
2025-12-02 11:58:05 -0300

    DTO dos usuarios

---
+++ backend/DTOs/UsuarioDTO.cs
@@ -0,0 +1,41 @@
+namespace backend.DTOs;
+using System.ComponentModel.DataAnnotations;
+using backend.Models;
+
+public class UsuarioResponseDTO
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string CPF { get; set; } = string.Empty;
+    public DateTime DataCadastro {get; set; }
+    public UsuarioResponseDTO(Usuarios u) {
+      Id = u.Id;
+      Nome = u.Nome;
+      Email = u.Email;
+      CPF = u.CPF;
+      DataCadastro = u.DataCadastro;
+    }
+}
+
+public class CriarUsuarioDTO
+{
+  [Required(ErrorMessage = "Nome do Usuario Necessario")]
+  public string Nome { get; set; } = string.Empty;
+
+  [Required(ErrorMessage = "Email do Usuario Necessario")]
+  public string Email { get; set; } = string.Empty;
+
+  [Required(ErrorMessage = "Senha do Usuario Necessario")]
+  public string Senha { get; set; } = string.Empty;
+
+  [Required(ErrorMessage = "CPF do Usuario Necessario")]
+  public string CPF { get; set; } = string.Empty;
+}
+
+public class AtualizarUsuarioDTO
+{
+  public string Nome { get; set; } = string.Empty;
+  public string Email { get; set; } = string.Empty;
+  public string CPF { get; set; } = string.Empty;
+}
