4794cb04f968e6237f82ead5bcb8731f08291595
GustavoSenna07 <gustavo.psenna@gmail.com>
2025-12-02 11:54:41 -0300

    migrations do banco de dados

---
+++ backend/Migrations/20251127132042_Initial.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "assentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SalaId = table.Column<int>(type: "integer", nullable: false),
+                    Fileira = table.Column<string>(type: "text", nullable: false),
+                    Coluna = table.Column<string>(type: "text", nullable: false),
+                    Tipo = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_assentos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "filmes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Titulo = table.Column<string>(type: "text", nullable: false),
+                    Genero = table.Column<string>(type: "text", nullable: false),
+                    Diretor = table.Column<string>(type: "text", nullable: false),
+                    Estudio = table.Column<string>(type: "text", nullable: false),
+                    DataLancamento = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DuracaoMinutos = table.Column<int>(type: "integer", nullable: false),
+                    Classificacao = table.Column<string>(type: "text", nullable: false),
+                    Sinopse = table.Column<string>(type: "text", nullable: false),
+                    ImagemURL = table.Column<string>(type: "text", nullable: false),
+                    Elenco = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_filmes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ingressos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UsuarioId = table.Column<int>(type: "integer", nullable: false),
+                    SessaoId = table.Column<int>(type: "integer", nullable: false),
+                    AssentoId = table.Column<int>(type: "integer", nullable: false),
+                    TipoIngresso = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false),
+                    DataCompra = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ValorTotal = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ingressos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "pagamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ReferenciaId = table.Column<int>(type: "integer", nullable: false),
+                    UsuarioId = table.Column<int>(type: "integer", nullable: false),
+                    Metodo = table.Column<int>(type: "integer", nullable: false),
+                    DataPagamento = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ValorTotal = table.Column<decimal>(type: "numeric", nullable: false),
+                    ReferenciaTipo = table.Column<string>(type: "text", nullable: false),
+                    CodigoTransacao = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_pagamentos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "salas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Numero = table.Column<int>(type: "integer", nullable: false),
+                    Capacidade = table.Column<int>(type: "integer", nullable: false),
+                    TipoSala = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_salas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "sessoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FilmeId = table.Column<int>(type: "integer", nullable: false),
+                    SalaId = table.Column<int>(type: "integer", nullable: false),
+                    Data = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    HorarioInicio = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    HorarioFim = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    Formato = table.Column<int>(type: "integer", nullable: false),
+                    AudioTipo = table.Column<int>(type: "integer", nullable: false),
+                    PrecoBase = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_sessoes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Senha = table.Column<string>(type: "text", nullable: false),
+                    CPF = table.Column<string>(type: "text", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_usuarios", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "assentos");
+
+            migrationBuilder.DropTable(
+                name: "filmes");
+
+            migrationBuilder.DropTable(
+                name: "ingressos");
+
+            migrationBuilder.DropTable(
+                name: "pagamentos");
+
+            migrationBuilder.DropTable(
+                name: "salas");
+
+            migrationBuilder.DropTable(
+                name: "sessoes");
+
+            migrationBuilder.DropTable(
+                name: "usuarios");
+        }
+    }
+}
